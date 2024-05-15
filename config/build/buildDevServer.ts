b96560072ac5581c39ee75aced5d5c2f6924bb06
mirzoibodullaev <1bodullaevm11@gmail.com>
2024-05-15 14:43:03 +0500

    add React and css loader

--- config/build/buildDevServer.ts
+++ config/build/buildDevServer.ts
@@ -1,6 +1,5 @@
 import { BuildOptions } from "./types/config";
-
-import { Configuration as DevServerConfiguration } from "webpack-dev-server";
+import type { Configuration as DevServerConfiguration } from "webpack-dev-server";
 
 export function buildDevServer(options: BuildOptions): DevServerConfiguration {
     return {
---
+++ src/index.tsx
@@ -0,0 +1,2 @@
+import { render } from "react-dom";
+render(<div>ads</div>, document.getElementById("root"));
