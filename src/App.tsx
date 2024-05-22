d22eafb70ef418b859460bfc5f5f5b7f2da8137d
mirzoibodullaev <1bodullaevm11@gmail.com>
2024-05-22 14:43:05 +0500

    add pages and lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
+import { Suspense } from "react";
+import { Link, Route, Routes } from "react-router-dom";
+import { MainPageAsync } from "./pages/MainPage/MainPage.async";
+import { AboutPageAync } from "./pages/AboutPage/AboutPage.async";
 import "./index.scss";
 
 const App = () => {
-    return <div className="app">App</div>;
+    return (
+        <div className="app">
+            <Link to={"/"}>Главная</Link>
+            <Link to={"/about"}>О Сайте</Link>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path="/" element={<MainPageAsync />} />
+                    <Route path="/about" element={<AboutPageAync />} />
+                </Routes>
+            </Suspense>
+        </div>
+    );
 };
 
 export default App;
