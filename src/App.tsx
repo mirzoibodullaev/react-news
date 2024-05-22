9717c7e13b1bb204cafc52edb9a69a23fd23f483
mirzoibodullaev <1bodullaevm11@gmail.com>
2024-05-22 15:38:08 +0500

    add theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,14 @@ import { Suspense } from "react";
 import { Link, Route, Routes } from "react-router-dom";
 import { MainPageAsync } from "./pages/MainPage/MainPage.async";
 import { AboutPageAync } from "./pages/AboutPage/AboutPage.async";
-import "./index.scss";
+import { useTheme } from "./theme/useTheme";
+import "./styles/index.scss";
 
 const App = () => {
+    const { theme, toggleTheme } = useTheme();
     return (
-        <div className="app">
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>Toggle</button>
             <Link to={"/"}>Главная</Link>
             <Link to={"/about"}>О Сайте</Link>
             <Suspense fallback={<div>Loading...</div>}>
