9b5beb96bec12bf4c32ba2d1cbc6922d963684b3
mirzoibodullaev <1bodullaevm11@gmail.com>
2024-06-03 16:38:03 +0500

    add SVGR loader, file loader, ThemeSwitcher and Button UI Kit

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -3,6 +3,11 @@ import webpack from "webpack";
 import { BuildOptions } from "./types/config";
 
 export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
+    const svgLoader = {
+        test: /\.svg$/,
+        use: ["@svgr/webpack"],
+    };
+
     const cssLoader = {
         test: /\.s[ac]ss$/i,
         use: [
@@ -11,7 +16,8 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
                 loader: "css-loader",
                 options: {
                     modules: {
-                        auto: (resPath: string) => Boolean(resPath.includes(".module")),
+                        auto: (resPath: string) =>
+                            Boolean(resPath.includes(".module")),
                         localIdentName: isDev
                             ? "[path][name]__[local]--[hash:base64:5]"
                             : "[hash:base64:8]",
@@ -27,5 +33,14 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
         use: "ts-loader",
         exclude: /node_modules/,
     };
-    return [typescriptLoader, cssLoader];
+
+    const fileLoader = {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+            {
+                loader: "file-loader",
+            },
+        ],
+    };
+    return [fileLoader, svgLoader, typescriptLoader, cssLoader];
 }
---
+++ src/shared/ui/Button/ui/Button.tsx
@@ -0,0 +1,25 @@
+import { classNames } from "shared/lib/classNames/classNames";
+import cls from "./Button.module.scss";
+import { ButtonHTMLAttributes, FC } from "react";
+
+export enum ThemeButton {
+    CLEAR = "clear",
+}
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+    className?: string;
+    theme?: ThemeButton;
+    onClick: () => void;
+}
+
+export const Button: FC<ButtonProps> = (props) => {
+    const { className, children, theme, ...otherProps } = props;
+
+    return (
+        <button
+            className={classNames(cls.Button, {}, [className, cls[theme]])}
+            {...otherProps}
+        >
+            {children}
+        </button>
+    );
+};
