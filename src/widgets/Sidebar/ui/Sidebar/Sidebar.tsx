a83480d01d4ff6b1552a8f0a18e431f1563a1c4c
mirzoibodullaev <1bodullaevm11@gmail.com>
2024-06-04 17:42:13 +0500

    add i18n and LangSwitcher

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { classNames } from "shared/lib/classNames/classNames";
 import cls from "./Sidebar.module.scss";
 import { ThemeSwitcher } from "shared/ui/ThemeSwitcher";
+import { LangSwitcher } from "shared/ui/LangSwitcher/ui/LangSwitcher";
 
 interface SidebarProps {
     className?: string;
@@ -22,6 +23,7 @@ export const Sidebar = ({ className }: SidebarProps) => {
             <button onClick={onToggle}>toggle</button>
             <div className={cls.switchers}>
                 <ThemeSwitcher />
+                <LangSwitcher className={cls.lang} />
             </div>
         </div>
     );
