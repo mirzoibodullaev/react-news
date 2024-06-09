299e76bc2734916ba93b39bf3b0ce1ac9fc204e4
mirzoibodullaev <1bodullaevm11@gmail.com>
2024-06-09 14:37:34 +0500

    add NotFoundPage and Loader

--- src/shared/config/routeConfig/routeConfig.tsx
+++ src/shared/config/routeConfig/routeConfig.tsx
@@ -1,24 +1,31 @@
 import { RouteProps } from "react-router-dom";
 import { AboutPage } from "pages/AboutPage";
 import { MainPage } from "pages/MainPage";
+import { NotFoundPage } from "pages/NotFoundPage";
 
 export enum AppRoutes {
     MAIN = "main",
     ABOUT = "about",
+    NOT_FOUND = "not_found",
 }
 
 export const RoutePaths: Record<AppRoutes, string> = {
     [AppRoutes.MAIN]: "/",
     [AppRoutes.ABOUT]: "/about",
+    [AppRoutes.NOT_FOUND]: "*",
 };
 
 export const routeConfig: Record<AppRoutes, RouteProps> = {
     [AppRoutes.MAIN]: {
         path: RoutePaths.main,
-        element: <MainPage/>
+        element: <MainPage />,
     },
     [AppRoutes.ABOUT]: {
         path: RoutePaths.about,
-        element: <AboutPage/>
-    }
-}
+        element: <AboutPage />,
+    },
+    [AppRoutes.NOT_FOUND]: {
+        path: RoutePaths.not_found,
+        element: <NotFoundPage />,
+    },
+};
